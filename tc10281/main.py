c573efe75ffc6d262bb765479f6a0c407131cb74
Sebastian Marines <sebastian0marines@gmail.com>
2021-10-16 11:57:52 -0500

    Draw buttons

--- tc10281/main.py
+++ tc10281/main.py
@@ -1,23 +1,52 @@
-import pyglet
-from string import ascii_lowercase
-
-window = pyglet.window.Window()
-
-label = pyglet.text.Label("Hello world", x=window.width // 2, y=window.height // 2)
+import tkinter as tk
+import tkinter.font as tkFont
+from tkinter import Button
+from string import ascii_uppercase
 
 
-@window.event
-def on_draw():
-    window.clear()
-    label.draw()
+class App:
+    def __init__(self, root):
+        # setting title
+        root.title("undefined")
+        # setting root size
+        width = 600
+        height = 500
+        screenwidth = root.winfo_screenwidth()
+        screenheight = root.winfo_screenheight()
+        alignstr = "%dx%d+%d+%d" % (
+            width,
+            height,
+            (screenwidth - width) / 2,
+            (screenheight - height) / 2,
+        )
+        root.geometry(alignstr)
+        root.resizable(width=True, height=True)
+
+        n = 13
+        letter_groups = [
+            ascii_uppercase[i : i + n] for i in range(0, len(ascii_uppercase), n)
+        ]
+        buttons = []
+        for i, group in enumerate(letter_groups, start=1):
+            for j, letter in enumerate(group, start=1):
+                _button = Button(
+                    root,
+                    text=letter,
+                    bg="skyBlue",
+                    fg="Black",
+                    width=3,
+                    height=1,
+                    font=("Helvetica", "20"),
+                    command=lambda: self.guess(letter),
+                )
+                _button.grid(column=j, row=i)
+                buttons.append(_button)
+
+    def guess(self, letter):
+        print(letter)
 
 
-@window.event
-def on_key_press(symbol: int, modifier: int):
-    letter = chr(symbol)
-    if letter not in ascii_lowercase:
-        return
-    label.text = letter
-
-
-pyglet.app.run()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
